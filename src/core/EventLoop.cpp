1647e222477d3f02b8890f4fd7eeb7fbc674643c
Caio Henrique Brito Santos <cbrito-s@c3r3p4.42sp.org.br>
2026-07-21 20:17:47 -0300

    Starting EventLoop(runOnce and reapClosed)

--- src/core/EventLoop.cpp
+++ src/core/EventLoop.cpp
@@ -1,6 +1,5 @@
 #include "core/EventLoop.hpp"
 
-
 EventLoop::EventLoop() : pollables_(), running_(false) {}
 EventLoop::~EventLoop() {}
 
@@ -10,13 +9,47 @@ void EventLoop::add(IPollable* pollable) {
 
 void EventLoop::remove(IPollable* pollable) {
 	for (std::vector<IPollable*>::iterator it = pollables_.begin();
-	     it != pollables_.end(); ++it) {
+		 it != pollables_.end(); ++it) {
 		if (*it == pollable) { pollables_.erase(it); return; }
 	}
 }
 
-void EventLoop::runOnce(int /*timeoutMs*/) {
-	// TODO Membro 1: montar pollfd[], poll(), despachar onReadable/onWritable/onHangup
+void EventLoop::runOnce(int timeoutMs) {
+	std::vector<pollfd> fds;
+	fds.reserve(pollables_.size());
+
+	for (std::vector<IPollable*>::iterator it = pollables_.begin();
+		 it != pollables_.end(); it++) {
+		IPollable* p = *it;
+
+		pollfd fd;
+		fd.fd = p->fd();
+		fd.events = p->interest();
+		fd.revents = 0;
+		fds.push_back(fd);
+	}
+
+	if (fds.empty())
+		return;
+
+	int return_poll = poll(&fds[0], fds.size(), timeoutMs);
+	if (return_poll < 0) {
+		if (errno == EINTR)
+			return;
+		throw std::runtime_error(std::string("POLL FAILED: ") + std::strerror(errno));
+	}
+
+	for (int i = 0; fds.size(); i++) {
+		IPollable* p = pollables_[i];
+		short revents = fds[i].revents;
+
+		if (revents & (POLLHUP | POLLERR))
+			p->onHangup();
+		else if (revents & POLLIN)
+			p->onReadable();
+		else if (revents & POLLOUT)
+			p->onWritable();
+	}
 }
 
 void EventLoop::run() {
@@ -31,6 +64,19 @@ void EventLoop::stop() { running_ = false; }
 bool EventLoop::isRunning() const { return running_; }
 
 void EventLoop::reapClosed() {
-	// TODO Membro 1: remover pollables com wantsClose() == true e deletar
+	for (std::vector<IPollable*>::iterator it = pollables_.begin();
+		 it != pollables_.end();) {
+		IPollable* p = *it;
+
+		if (p->wantsClose() == true) {
+			std::ostringstream oss;
+			oss << "DELETE FD " << p->fd();
+			LOG_INFO(oss.str());
+			it = pollables_.erase(it);
+			delete p;
+		}
+		else
+			it++;
+	}
 }
 
