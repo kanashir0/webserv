1908860a5915e8c6bb05b7c245a2aeafedcb078c
Caio Santos <caiohbsantos@hotmail.com>
2026-07-28 15:20:32 -0300

    refactor: remove namespaces

--- src/http/ResponseFactory.cpp
+++ src/http/ResponseFactory.cpp
@@ -5,70 +5,67 @@
 #include <sstream>
 #include <sys/stat.h>
 
-namespace {
+static bool readRegularFile(const std::string& path, std::string& outContent) {
+	if (path.empty()) {
+		return false;
+	}
+	struct stat st;
+	if (stat(path.c_str(), &st) != 0 || !S_ISREG(st.st_mode)) {
+		return false;
+	}
+	std::ifstream in(path.c_str(), std::ios::in | std::ios::binary);
+	if (!in.is_open()) {
+		return false;
+	}
+	std::ostringstream buf;
+	buf << in.rdbuf();
+	if (in.bad()) {
+		return false;
+	}
+	outContent = buf.str();
+	return true;
+}
 
-	bool readRegularFile(const std::string& path, std::string& outContent) {
-		if (path.empty()) {
-			return false;
-		}
-		struct stat st;
-		if (stat(path.c_str(), &st) != 0 || !S_ISREG(st.st_mode)) {
-			return false;
-		}
-		std::ifstream in(path.c_str(), std::ios::in | std::ios::binary);
-		if (!in.is_open()) {
-			return false;
-		}
-		std::ostringstream buf;
-		buf << in.rdbuf();
-		if (in.bad()) {
-			return false;
-		}
-		outContent = buf.str();
+static std::string joinPath(const std::string& root, const std::string& rel) {
+	if (root.empty()) {
+		return rel;
+	}
+	std::string joined = root;
+	bool rootHasSlash = (joined[joined.size() - 1] == '/');
+	bool relHasSlash  = (!rel.empty() && rel[0] == '/');
+	if (rootHasSlash && relHasSlash) {
+		joined.erase(joined.size() - 1);
+	} else if (!rootHasSlash && !relHasSlash) {
+		joined += '/';
+	}
+	joined += rel;
+	return joined;
+}
+
+static bool loadConfiguredErrorPage(int code, const ServerConfig& cfg, std::string& outBody) {
+	std::map<int, std::string>::const_iterator it = cfg.errorPages.find(code);
+	if (it == cfg.errorPages.end() || it->second.empty()) {
+		return false;
+	}
+	const std::string& pageUri = it->second;
+	const LocationConfig* loc = cfg.findLocation(pageUri);
+	if (loc && !loc->root.empty() &&
+		readRegularFile(joinPath(loc->root, pageUri), outBody)) {
 		return true;
 	}
+	return readRegularFile(pageUri, outBody);
+}
 
-	std::string joinPath(const std::string& root, const std::string& rel) {
-		if (root.empty()) {
-			return rel;
-		}
-		std::string joined = root;
-		bool rootHasSlash = (joined[joined.size() - 1] == '/');
-		bool relHasSlash  = (!rel.empty() && rel[0] == '/');
-		if (rootHasSlash && relHasSlash) {
-			joined.erase(joined.size() - 1);
-		} else if (!rootHasSlash && !relHasSlash) {
-			joined += '/';
-		}
-		joined += rel;
-		return joined;
-	}
-
-	bool loadConfiguredErrorPage(int code, const ServerConfig& cfg, std::string& outBody) {
-		std::map<int, std::string>::const_iterator it = cfg.errorPages.find(code);
-		if (it == cfg.errorPages.end() || it->second.empty()) {
-			return false;
-		}
-		const std::string& pageUri = it->second;
-		const LocationConfig* loc = cfg.findLocation(pageUri);
-		if (loc && !loc->root.empty() &&
-			readRegularFile(joinPath(loc->root, pageUri), outBody)) {
-			return true;
-		}
-		return readRegularFile(pageUri, outBody);
-	}
-
-	std::string makeBuiltinErrorPage(int code) {
-		std::string title = StringUtils::toString(code) + " " + statusReason(code);
-		return "<html>\r\n"
-			   "<head><title>" + title + "</title></head>\r\n"
-			   "<body>\r\n"
-			   "<h1>" + title + "</h1>\r\n"
-			   "<hr>\r\n"
-			   "<p>webserv</p>\r\n"
-			   "</body>\r\n"
-			   "</html>\r\n";
-	}
+static std::string makeBuiltinErrorPage(int code) {
+	std::string title = StringUtils::toString(code) + " " + statusReason(code);
+	return "<html>\r\n"
+		   "<head><title>" + title + "</title></head>\r\n"
+		   "<body>\r\n"
+		   "<h1>" + title + "</h1>\r\n"
+		   "<hr>\r\n"
+		   "<p>webserv</p>\r\n"
+		   "</body>\r\n"
+		   "</html>\r\n";
 }
 
 Response ResponseFactory::makeError(int code, const ServerConfig& cfg) {
